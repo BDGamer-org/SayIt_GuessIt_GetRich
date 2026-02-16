3dc0f1d78d89c6ba3773eab9a2bae83a63869e2e
RRrrChloe <ruonanzh@unc.edu>
2026-02-16 09:12:55 -0500

    chore: update components/screens/AuthScreen.vue

--- components/screens/AuthScreen.vue
+++ components/screens/AuthScreen.vue
@@ -65,14 +65,14 @@ export default {
 }
 
 .card-main-title {
-  font-size: 32px;
+  font-size: var(--modal-title-size, 32px);
   font-weight: bold;
   color: #333;
   margin-bottom: 8px;
 }
 
 .card-subtitle {
-  font-size: 14px;
+  font-size: var(--modal-subtitle-size, 14px);
   color: #666;
   margin-bottom: 25px;
 }
